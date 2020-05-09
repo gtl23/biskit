9e25542239bdc53799e6b81142717e1ef6fc2a8f
Gaurav Atul <asperanza23@gmail.com>
2020-05-09 10:57:19 +0530

    init commit

---
+++ src/main/java/com/project/biskit/BiskitApplication.java
@@ -0,0 +1,13 @@
+package com.project.biskit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiskitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiskitApplication.class, args);
+	}
+
+}
